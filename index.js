64fbe5b175c98c4c95c949d077ff05353596f788
belaeat <omyhossain@gmail.com>
2023-05-04 11:40:08 +0600

    data response are accurate

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-app.get('/', (req, res) =>{
+const chef = require('./data/chef.json')
+
+const recipes = require('./data/recipes.json')
+
+app.get('/', (req, res) => {
     res.send("Dragon in running")
 })
 
-app.listen(port, ()=>{
+// Chef's data load
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
+
+// Recipes data load
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes)
+})
+
+
+
+app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`)
 })
