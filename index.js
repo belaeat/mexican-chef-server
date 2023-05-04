b5a4d4c942f70bae18a4b2f5f227d288153240b8
belaeat <omyhossain@gmail.com>
2023-05-04 09:59:23 +0600

    server side linked and initialized

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+app.get('/', (req, res) =>{
+    res.send("Dragon in running")
+})
+
+app.listen(port, ()=>{
+    console.log(`Dragon API is running on port: ${port}`)
+})
